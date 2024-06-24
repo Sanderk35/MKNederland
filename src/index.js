0a92c1fbdbd977281f64e149ced4c319ce74c014
Sanderk35 <76066612+Sanderk35@users.noreply.github.com>
2024-06-24 16:15:21 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -6,4 +6,7 @@ require("dotenv").config();
 
 const client = new Client({intents: [IntentsBitField.Flags.Guilds, IntentsBitField.Flags.GuildMessages]});
 
+const eventsPath = path.join(__dirname, "events");
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
+
 client.login(process.env.TOKEN);
